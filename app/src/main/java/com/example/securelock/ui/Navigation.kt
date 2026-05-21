745cd47fabb1b42b9725b0ba8fd9d4229e63db10
AndreaScuola <162697678+AndreaScuola@users.noreply.github.com>
2026-05-21 08:08:51 +0200

    Commenti a Navigation + Rimosso AdminVehicleActionSelector.kt

--- app/src/main/java/com/example/securelock/ui/Navigation.kt
+++ app/src/main/java/com/example/securelock/ui/Navigation.kt
@@ -7,21 +7,30 @@ import androidx.navigation.compose.rememberNavController
 import com.example.securelock.ui.admin.AdminNewUserScreen
 import com.example.securelock.ui.admin.AdminVehicleScreen
 
+// Oggetto (container di costanti)
+// Routes definite => parentesi graffe per passaggio dati
 object Routes {
     const val HOME = "home"
     const val FACE_AUTH = "face_auth"
     const val LOGIN = "login"
     const val CREDITS = "credits"
+    const val CREATE_USER = "admin_new_user/{adminId}"
+    const val WELCOME = "welcome/{userId}/{isAdmin}"
     const val SETUP = "setup/{superAdminId}"
     const val SLOT_DETAIL = "slot_detail/{userId}/{deviceId}/{slotId}"
+    const val ADMIN_VEHICLES = "admin_vehicles/{userId}"
 }
 
+// UI di compose (prima creata)
 @Composable
 fun AppNavigation() {
+    // Crea NavController per stack delle schermate
+    // Permette di navigare tra schermate
     val navController = rememberNavController()
-
     val startDestination = Routes.HOME
 
+    // Container che collega il navController con le schermate composable
+    // Si passa navController ad ogni pagina per i redirect
     NavHost(
         navController = navController,
         startDestination = startDestination
@@ -42,33 +51,43 @@ fun AppNavigation() {
             CreditsScreen(navController = navController)
         }
 
-        composable("setup/{superAdminId}") { backStackEntry ->
+        // Passaggio dati tramite route
+        composable(Routes.SETUP) { backStackEntry ->
+            // backStackEntry contiene dati e stati (cerca ID superadmin)
             val superAdminId = backStackEntry.arguments
                 ?.getString("superAdminId")
                 ?.toIntOrNull() ?: 0
 
+            // Apri la schermata e passa l'ID con una callback (event)
+            // redirect a loginScreen
             SetupScreen(
                 superAdminId = superAdminId,
                 onSetupCompleted = {
-                    navController.navigate(Routes.LOGIN) {
-                        popUpTo(Routes.LOGIN) { inclusive = true }
+                    // Apri homescreen ed elimina dati dal backstack
+                    navController.navigate(Routes.HOME) {
+                        popUpTo(Routes.HOME) { inclusive = true }
                     }
                 }
             )
         }
 
-        composable("admin_new_user/{adminId}") { backStackEntry ->
+        // Passaggio dati tramite route => adminID
+        composable(Routes.CREATE_USER) { backStackEntry ->
             val adminId = backStackEntry.arguments
                 ?.getString("adminId")
                 ?.toIntOrNull() ?: -1
 
+            // Schermata per la gestione utenti
             AdminNewUserScreen(
                 navController = navController,
                 userId = adminId
             )
         }
 
-        composable("welcome/{userId}/{isAdmin}") { backStackEntry ->
+        // Dati route:
+        // * userID
+        // * isAdmin
+        composable(Routes.WELCOME) { backStackEntry ->
             val userId = backStackEntry.arguments
                 ?.getString("userId")
                 ?.toIntOrNull() ?: 0
@@ -77,6 +96,7 @@ fun AppNavigation() {
                 ?.getString("isAdmin")
                 ?.toBoolean() ?: false
 
+            // Schermata principale app
             WelcomeScreen(
                 userId = userId,
                 isAdmin = isAdmin,
@@ -84,7 +104,11 @@ fun AppNavigation() {
             )
         }
 
-        composable("slot_detail/{userId}/{deviceId}/{slotId}") { backStackEntry ->
+        // Dati route:
+        // * userID
+        // * deviceID => id univoco dell'ESP32
+        // * slotID => id del drawer da aprire
+        composable(Routes.SLOT_DETAIL) { backStackEntry ->
             val userId = backStackEntry.arguments
                 ?.getString("userId")
                 ?.toIntOrNull() ?: 0
@@ -97,6 +121,7 @@ fun AppNavigation() {
                 ?.getString("slotId")
                 ?.toIntOrNull() ?: 0
 
+            // Schermata con informazioni drawer e aggiornamenti stati
             SlotDetailScreen(
                 userId = userId,
                 deviceId = deviceId,
@@ -105,11 +130,13 @@ fun AppNavigation() {
             )
         }
 
-        composable("admin_vehicles/{userId}") { backStackEntry ->
+        // Dati route: userID
+        composable(Routes.ADMIN_VEHICLES) { backStackEntry ->
             val userId = backStackEntry.arguments
                 ?.getString("userId")
                 ?.toIntOrNull() ?: 0
 
+            // Schermata gestione veicoli
             AdminVehicleScreen(
                 navController = navController,
                 userId = userId
